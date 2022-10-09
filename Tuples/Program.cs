f8e93e96dcdffc28e780c06a797d04a5d23b626b
elena <chayori@icloud.com>
2022-10-09 12:00:50 +0300

    Кортежи

---
+++ Tuples/Program.cs
@@ -0,0 +1,49 @@
+﻿// кортежи
+
+int Max(int [] array)
+{
+    int max = array[0];
+for (int i = 1; i < array.length; i++)
+{
+    if(array[i] > max)
+    max = array[i];
+}
+return max;
+}
+
+int Min(int [] array)
+{
+    int min = array[0];
+for (int i = 1; i < array.length; i++)
+{
+    if(array[i] < min)
+    min = array[i];
+}
+return min;
+}
+
+
+// первым возвращается max вторым min
+(int, int) FindMaxAndMin(int []array)
+{
+    int max = array[0];
+    int min = array[0];
+    for (int i = 1; i < array.length; i++)
+{
+    if(array[i] > max)
+    max = array[i];
+   
+    if(array[i] < min)
+    min = array[i];
+}
+
+return(max, min);
+
+// void, int, string, bool, random, кастомные классы из разных библиотек
+
+int [] array = new int[10];
+
+(int max, int min) = FindMaxAndMin;
+
+if(max > min)
+Console.WriteLine($"{max}, {min} Макс больше мин");
