fa4a5cc013e3619fad720fbc356f4649e22ee5a0
elena <chayori@icloud.com>
2022-10-09 13:17:08 +0300

    Квадраты до заданного числа

---
+++ Task4/Program.cs
@@ -0,0 +1,32 @@
+﻿/* Напишите программу, которая принимает на вход число N и выдает таблицу квадратов от 1 до N.
+*/
+int GetNumber(string message)
+{
+    int result = 0;
+    bool isCorrect = false;
+
+    while (!isCorrect)
+    {
+        Console.WriteLine(message);
+
+        if (int.TryParse(Console.ReadLine(), out result) )
+            isCorrect = true;
+        else
+            Console.WriteLine("Введите валидную координату!\n");
+
+    }
+    return result;
+}
+
+void GetNumb (int number)
+{
+    Console.Write($"{number} ->");
+    for (int i = 1; i <= number; i++) 
+    {
+        double square = Math.Pow(i,2);
+        Console.WriteLine($" {square},");
+    }
+}
+Console.WriteLine("Введите число: ");
+int numb = int.Parse(Console.ReadLine());
+GetNumb (numb);
